f1c8d9fe100374607364499e3d4f40511c9691a8
miDaya02 <dannyjair112@gmail.com>
2025-09-25 16:15:48 -0500

    feat: :sparkles: The Overview and Contacts views were created

---
+++ src/app/contacts/page.tsx
@@ -0,0 +1,89 @@
+export default function Contacts() {
+  return (
+    <section className="card">
+      <h2>Contact List</h2>
+
+      <div className="cards-container">
+        <div className="contact-card">
+          <img src="/avatar.png" alt="avatar" className="avatar" />
+          <div className="info">
+            <h3>Full Name</h3>
+            <p>email@email.com</p>
+          </div>
+          <div className="actions">
+            <button className="favorite">
+              <img src="/favorite.svg" className="iconFavorite" alt="favorite" />
+            </button>
+            <button className="remove">
+              <img src="/x.svg" className="iconx" alt="remove" />
+            </button>
+          </div>
+        </div>
+
+        <div className="contact-card">
+          <img src="/avatar.png" alt="avatar" className="avatar" />
+          <div className="info">
+            <h3>Full Name</h3>
+            <p>email@email.com</p>
+          </div>
+          <div className="actions">
+            <button className="favorite">
+              <img src="/favorite.svg" className="iconFavorite" alt="favorite" />
+            </button>
+            <button className="remove">
+              <img src="/x.svg" className="iconx" alt="remove" />
+            </button>
+          </div>
+        </div>
+
+        <div className="contact-card">
+          <img src="/avatar.png" alt="avatar" className="avatar" />
+          <div className="info">
+            <h3>Full Name</h3>
+            <p>email@email.com</p>
+          </div>
+          <div className="actions">
+            <button className="favorite">
+              <img src="/favorite.svg" className="iconFavorite" alt="favorite" />
+            </button>
+            <button className="remove">
+              <img src="/x.svg" className="iconx" alt="remove" />
+            </button>
+          </div>
+        </div>
+
+        <div className="contact-card">
+          <img src="/avatar.png" alt="avatar" className="avatar" />
+          <div className="info">
+            <h3>Full Name</h3>
+            <p>email@email.com</p>
+          </div>
+          <div className="actions">
+            <button className="favorite">
+              <img src="/favorite.svg" className="iconFavorite" alt="favorite" />
+            </button>
+            <button className="remove">
+              <img src="/x.svg" className="iconx" alt="remove" />
+            </button>
+          </div>
+        </div>
+
+        <div className="contact-card">
+          <img src="/avatar.png" alt="avatar" className="avatar" />
+          <div className="info">
+            <h3>Full Name</h3>
+            <p>email@email.com</p>
+          </div>
+          <div className="actions">
+            <button className="favorite">
+              <img src="/favorite.svg" className="iconFavorite" alt="favorite" />
+            </button>
+            <button className="remove">
+              <img src="/x.svg" className="iconx" alt="remove" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
