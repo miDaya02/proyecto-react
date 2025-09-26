9a99e92b32532caacd8e630536829e214aacb05d
miDaya02 <dannyjair112@gmail.com>
2025-09-26 10:56:28 -0500

    fix: :bug: Correction in the trash icon in the contact list

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -11,11 +11,11 @@ export default function Contacts() {
             <p>email@email.com</p>
           </div>
           <div className="actions">
-            <button className="favorite">
-              <img src="/favorite.svg" className="iconFavorite" alt="favorite" />
-            </button>
             <button className="remove">
               <img src="/x.svg" className="iconx" alt="remove" />
+            </button>
+            <button className="trash">
+              <img src="/trash.svg" className="iconTrash" alt="trash" />
             </button>
           </div>
         </div>
@@ -27,11 +27,39 @@ export default function Contacts() {
             <p>email@email.com</p>
           </div>
           <div className="actions">
-            <button className="favorite">
-              <img src="/favorite.svg" className="iconFavorite" alt="favorite" />
-            </button>
             <button className="remove">
               <img src="/x.svg" className="iconx" alt="remove" />
+            </button>
+            <button className="trash">
+              <img src="/trash.svg" className="iconTrash" alt="trash" />
+            </button>
+          </div>
+        </div>
+        <div className="contact-card">
+          <img src="/avatar.png" alt="avatar" className="avatarc" />
+          <div className="info">
+            <h3>Full Name</h3>
+            <p>email@email.com</p>
+          </div>
+          <div className="actions">
+            <button className="favorite"><img src="/favorite.svg" className="iconFavorite"></img></button>
+            <button className="trash">
+              <img src="/trash.svg" className="iconTrash" alt="trash" />
+            </button>
+          </div>
+        </div>
+
+        <div className="contact-card">
+          <img src="/avatar.png" alt="avatar" className="avatarc" />
+          <div className="info">
+            <h3>Full Name</h3>
+            <p>email@email.com</p>
+          </div>
+
+          <div className="actions">
+            <button className="favorite"><img src="/favorite.svg" className="iconFavorite"></img></button>
+            <button className="trash">
+              <img src="/trash.svg" className="iconTrash" alt="trash" />
             </button>
           </div>
         </div>
@@ -43,11 +71,69 @@ export default function Contacts() {
             <p>email@email.com</p>
           </div>
           <div className="actions">
-            <button className="favorite">
-              <img src="/favorite.svg" className="iconFavorite" alt="favorite" />
-            </button>
             <button className="remove">
               <img src="/x.svg" className="iconx" alt="remove" />
+            </button>
+            <button className="trash">
+              <img src="/trash.svg" className="iconTrash" alt="trash" />
+            </button>
+          </div>
+        </div>
+        <div className="contact-card">
+          <img src="/avatar.png" alt="avatar" className="avatarc" />
+          <div className="info">
+            <h3>Full Name</h3>
+            <p>email@email.com</p>
+          </div>
+          <div className="actions">
+            <button className="favorite"><img src="/favorite.svg" className="iconFavorite"></img></button>
+            <button className="trash">
+              <img src="/trash.svg" className="iconTrash" alt="trash" />
+            </button>
+          </div>        </div>
+
+        <div className="contact-card">
+          <img src="/avatar.png" alt="avatar" className="avatarc" />
+          <div className="info">
+            <h3>Full Name</h3>
+            <p>email@email.com</p>
+          </div>
+          <button className="favorite"><img src="/favorite.svg" className="iconFavorite"></img></button>
+        </div>
+        <div className="contact-card">
+          <img src="/avatar.png" alt="avatar" className="avatarc" />
+          <div className="info">
+            <h3>Full Name</h3>
+            <p>email@email.com</p>
+          </div>
+          <div className="actions">
+            <button className="favorite"><img src="/favorite.svg" className="iconFavorite"></img></button>
+            <button className="trash">
+              <img src="/trash.svg" className="iconTrash" alt="trash" />
+            </button>
+          </div>
+        </div>
+
+        <div className="contact-card">
+          <img src="/avatar.png" alt="avatar" className="avatarc" />
+          <div className="info">
+            <h3>Full Name</h3>
+            <p>email@email.com</p>
+          </div>
+          <button className="favorite"><img src="/favorite.svg" className="iconFavorite"></img></button>
+        </div>
+        <div className="contact-card">
+          <img src="/avatar.png" alt="avatar" className="avatar" />
+          <div className="info">
+            <h3>Full Name</h3>
+            <p>email@email.com</p>
+          </div>
+          <div className="actions">
+            <button className="remove">
+              <img src="x.svg" className="iconx" alt="remove" />
+            </button>
+            <button className="trash">
+              <img src="/trash.svg" className="iconTrash" alt="trash" />
             </button>
           </div>
         </div>
@@ -59,27 +145,11 @@ export default function Contacts() {
             <p>email@email.com</p>
           </div>
           <div className="actions">
-            <button className="favorite">
-              <img src="/favorite.svg" className="iconFavorite" alt="favorite" />
-            </button>
             <button className="remove">
               <img src="/x.svg" className="iconx" alt="remove" />
             </button>
-          </div>
-        </div>
-
-        <div className="contact-card">
-          <img src="/avatar.png" alt="avatar" className="avatar" />
-          <div className="info">
-            <h3>Full Name</h3>
-            <p>email@email.com</p>
-          </div>
-          <div className="actions">
-            <button className="favorite">
-              <img src="/favorite.svg" className="iconFavorite" alt="favorite" />
-            </button>
-            <button className="remove">
-              <img src="/x.svg" className="iconx" alt="remove" />
+            <button className="trash">
+              <img src="/trash.svg" className="iconTrash" alt="trash" />
             </button>
           </div>
         </div>
